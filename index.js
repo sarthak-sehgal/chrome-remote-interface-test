492f09c6e59ba3957b8a8908c1b1ac92233f7b35
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2019-06-27 15:41:42 +0530

    Updated tracing config

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 
 CDP(async (client) => {
     try {
+        const traceConfig = {
+            includedCategories: ['-*', 'devtools.timeline', 'v8.execute', 'disabled-by-default-devtools.timeline',
+                'disabled-by-default-devtools.timeline.frame', 'toplevel',
+                'blink.console', 'blink.user_timing', 'latencyInfo', 'disabled-by-default-devtools.timeline.stack',
+                'disabled-by-default-v8.cpu_profiler', 'disabled-by-default-v8.cpu_profiler.hires'
+                ],
+                enableSampling: true,
+                enableSystrace: true
+        };
         const {Page, Tracing} = client;
         // enable Page domain events
         await Page.enable();
@@ -11,13 +20,13 @@ CDP(async (client) => {
         Tracing.dataCollected(({value}) => {
             events.push(...value);
         });
-        await Tracing.start();
+        await Tracing.start(traceConfig);
         await Page.navigate({url: 'https://github.com'});
         await Page.loadEventFired();
         await Tracing.end();
         await Tracing.tracingComplete();
         // save the tracing data
-        fs.writeFileSync('./GithubProfileUsingCDP.json', JSON.stringify(events, null, 2));
+        fs.writeFileSync('./GithubProfileUsingCDP.json', JSON.stringify(events));
     } catch (err) {
         console.error(err);
     } finally {
